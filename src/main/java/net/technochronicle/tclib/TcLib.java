bf4343681c9ffa9fa477f1306ef79dea1d9369b2
LeeQianXi <qianxilee232@163.com>
2026-01-04 10:52:10 +0800

    init commit

---
+++ src/main/java/net/technochronicle/tclib/TcLib.java
@@ -0,0 +1,13 @@
+package net.technochronicle.tclib;
+
+import net.neoforged.fml.common.Mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(TcLib.MOD_ID)
+public class TcLib {
+
+    public static final String MOD_ID = "tclib";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+}
